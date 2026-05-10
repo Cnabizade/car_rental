7f2b7a1b79670d8c67a9861a8186e76c79ca02c3
Cnabizade <cnabizada9452@gmail.com>
2026-05-10 13:05:16 +0300

    Update Car.h

--- Car.h
+++ Car.h
@@ -7,7 +7,8 @@ class Car : public Vehicle {
 protected:
     int seats;
 public:
-    Car(std::string plate, double rate, int s) : Vehicle(plate, rate), seats(s) {}
+    Car(std::string plate, double rate, int s) 
+        : Vehicle(plate, rate), seats(s) {}
 };
 
 #endif
