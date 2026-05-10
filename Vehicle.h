624dbdc250c4440c098f5042642e442f55af7319
Cnabizade <cnabizada9452@gmail.com>
2026-05-10 12:53:14 +0300

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -1,5 +1,6 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
+
 #include <string>
 
 class Vehicle {
@@ -9,15 +10,20 @@ protected:
     bool isRented;
 
 public:
-    Vehicle(std::string plate, double rate);
+    Vehicle(std::string plate, double rate) 
+        : licensePlate(plate), dailyRate(rate), isRented(false) {}
+
     virtual ~Vehicle() = default;
 
-    virtual double calculateCost(int days) const = 0; // Pure virtual
-    virtual std::string getDescription() const = 0;
+    bool getIsRented() const { return isRented; }
     
+    void setRented(bool status) { isRented = status; }
+
+    virtual double calculateCost(int days) const = 0;
+    virtual std::string getDescription() const = 0;
+
     std::string getPlate() const { return licensePlate; }
     double getRate() const { return dailyRate; }
-    bool getStatus() const { return isRented; }
-    void setRented(bool status) { isRented = status; }
 };
+
 #endif
